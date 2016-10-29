6670d97e34bbd1997fecf5270e33aee3f4e43b68
jatin-jt <sachinjatingupta@gmail.com>
2016-10-29 14:20:40 +0530

    input form for individual expense opened suitable dialog pickers

--- app/src/main/java/com/aanyajindal/splitexpense/AddIndividualExpense.java
+++ app/src/main/java/com/aanyajindal/splitexpense/AddIndividualExpense.java
@@ -3,13 +3,13 @@ package com.aanyajindal.splitexpense;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
@@ -19,9 +19,9 @@ import java.util.Calendar;
 
 public class AddIndividualExpense extends AppCompatActivity {
 
-    EditText etInputMode, etInputCategory, etInputDate, etInputTime;
+    EditText etInputMode, etInputCategory, etInputDate, etInputTime, etInputTags, etInputDesc, etInputAmount, etInputTitle;
     Calendar myCalendar = Calendar.getInstance();
-
+    Button btnSaveIndividualExpense, btnCancelIndividualExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +32,14 @@ public class AddIndividualExpense extends AppCompatActivity {
         getSupportActionBar().setTitle("Add Expense");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+        btnSaveIndividualExpense = (Button) findViewById(R.id.btn_save_individual_expense);
+        btnCancelIndividualExpense = (Button) findViewById(R.id.btn_cancel_individual_expense);
+
+        etInputAmount = (EditText) findViewById(R.id.et_input_amount);
+        etInputTitle = (EditText) findViewById(R.id.et_input_title);
+        etInputDesc = (EditText) findViewById(R.id.et_input_desc);
+        etInputTags = (EditText) findViewById(R.id.et_input_tags);
         etInputMode = (EditText) findViewById(R.id.et_input_mode);
         etInputCategory = (EditText) findViewById(R.id.et_input_category);
         etInputTime = (EditText) findViewById(R.id.et_input_time);
@@ -44,43 +52,45 @@ public class AddIndividualExpense extends AppCompatActivity {
                 openModeDialog();
             }
         });
-
         etInputCategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openCategoryDialog();
             }
         });
-
         etInputDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new DatePickerDialog(AddIndividualExpense.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        myCalendar.set(year,month,dayOfMonth);
-                        String myFormat = "MM/dd/yy";
-                        SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
-                        etInputDate.setText(sdf.format(myCalendar.getTime()));
-                    }
-                },myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                openDatePickerDialog();
             }
         });
-
         etInputTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new TimePickerDialog(AddIndividualExpense.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        myCalendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                        myCalendar.set(Calendar.MINUTE,minute);
-                        etInputTime.setText(hourOfDay+":"+minute);
-                    }
-                },myCalendar.get(Calendar.HOUR_OF_DAY),myCalendar.get(Calendar.MINUTE),false).show();
+                openTimePickerDialog();
             }
         });
 
+        btnSaveIndividualExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = etInputTitle.getText().toString();
+                String amount = etInputAmount.getText().toString();
+                String desc  = etInputDesc.getText().toString();
+                String tags = etInputTags.getText().toString();
+                String mode = etInputMode.getText().toString();
+                String category = etInputCategory.getText().toString();
+                String date = etInputDate.getText().toString();
+                String time = etInputTime.getText().toString();
+            }
+        });
+        btnCancelIndividualExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(AddIndividualExpense.this,HomeActivity.class));
+            }
+        });
     }
 
     public void openModeDialog() {
@@ -106,7 +116,7 @@ public class AddIndividualExpense extends AppCompatActivity {
     public void openCategoryDialog() {
         AlertDialog.Builder category = new AlertDialog.Builder(AddIndividualExpense.this);
         category.setTitle("Choose category");
-        final String[] categories = {"Food", "Transportation","Bills and utilities","Medical","Household","Clothes","Gifts", "Education","Leisure"};
+        final String[] categories = {"Food", "Transportation", "Bills and utilities", "Medical", "Household", "Clothes", "Gifts", "Education", "Leisure"};
         category.setSingleChoiceItems(categories, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -121,6 +131,29 @@ public class AddIndividualExpense extends AppCompatActivity {
         });
         category.setNegativeButton("Cancel", null);
         category.create().show();
+    }
+
+    public void openDatePickerDialog() {
+        new DatePickerDialog(AddIndividualExpense.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(year, month, dayOfMonth);
+                String myFormat = "MM/dd/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
+                etInputDate.setText(sdf.format(myCalendar.getTime()));
+            }
+        }, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    public void openTimePickerDialog() {
+        new TimePickerDialog(AddIndividualExpense.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                myCalendar.set(Calendar.MINUTE, minute);
+                etInputTime.setText(hourOfDay + ":" + minute);
+            }
+        }, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), false).show();
     }
 
 }
